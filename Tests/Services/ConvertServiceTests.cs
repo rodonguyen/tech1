3e5a4cede397bd82c5145fcda7995f32c3f77532
Rodo <rodonguyendd@gmail.com>
2025-07-30 00:35:31 +1000

    tests: fix to use Uppercase

--- Tests/Services/ConvertServiceTests.cs
+++ Tests/Services/ConvertServiceTests.cs
@@ -16,16 +16,16 @@ public class ConvertServiceTests
     #region Basic components
 
     [Theory]
-    [InlineData(0, "Zero Dollar")]
-    [InlineData(1, "One Dollar")]
-    [InlineData(2, "Two Dollars")]
-    [InlineData(3, "Three Dollars")]
-    [InlineData(4, "Four Dollars")]
-    [InlineData(5, "Five Dollars")]
-    [InlineData(6, "Six Dollars")]
-    [InlineData(7, "Seven Dollars")]
-    [InlineData(8, "Eight Dollars")]
-    [InlineData(9, "Nine Dollars")]
+    [InlineData(0, "ZERO DOLLAR")]
+    [InlineData(1, "ONE DOLLAR")]
+    [InlineData(2, "TWO DOLLARS")]
+    [InlineData(3, "THREE DOLLARS")]
+    [InlineData(4, "FOUR DOLLARS")]
+    [InlineData(5, "FIVE DOLLARS")]
+    [InlineData(6, "SIX DOLLARS")]
+    [InlineData(7, "SEVEN DOLLARS")]
+    [InlineData(8, "EIGHT DOLLARS")]
+    [InlineData(9, "NINE DOLLARS")]
     public void ConvertToWords_SingleDigits_ReturnsCorrectWords(decimal input, string expected)
     {
         var result = _convertService.ConvertCurrencyAmountToWords(input);
@@ -33,16 +33,16 @@ public class ConvertServiceTests
     }
 
     [Theory]
-    [InlineData(10, "Ten Dollars")]
-    [InlineData(11, "Eleven Dollars")]
-    [InlineData(12, "Twelve Dollars")]
-    [InlineData(13, "Thirteen Dollars")]
-    [InlineData(14, "Fourteen Dollars")]
-    [InlineData(15, "Fifteen Dollars")]
-    [InlineData(16, "Sixteen Dollars")]
-    [InlineData(17, "Seventeen Dollars")]
-    [InlineData(18, "Eighteen Dollars")]
-    [InlineData(19, "Nineteen Dollars")]
+    [InlineData(10, "TEN DOLLARS")]
+    [InlineData(11, "ELEVEN DOLLARS")]
+    [InlineData(12, "TWELVE DOLLARS")]
+    [InlineData(13, "THIRTEEN DOLLARS")]
+    [InlineData(14, "FOURTEEN DOLLARS")]
+    [InlineData(15, "FIFTEEN DOLLARS")]
+    [InlineData(16, "SIXTEEN DOLLARS")]
+    [InlineData(17, "SEVENTEEN DOLLARS")]
+    [InlineData(18, "EIGHTEEN DOLLARS")]
+    [InlineData(19, "NINETEEN DOLLARS")]
     public void ConvertToWords_TeenNumbers_ReturnsCorrectWords(decimal input, string expected)
     {
         var result = _convertService.ConvertCurrencyAmountToWords(input);
@@ -50,14 +50,14 @@ public class ConvertServiceTests
     }
 
     [Theory]
-    [InlineData(20, "Twenty Dollars")]
-    [InlineData(30, "Thirty Dollars")]
-    [InlineData(40, "Forty Dollars")]
-    [InlineData(50, "Fifty Dollars")]
-    [InlineData(60, "Sixty Dollars")]
-    [InlineData(70, "Seventy Dollars")]
-    [InlineData(80, "Eighty Dollars")]
-    [InlineData(90, "Ninety Dollars")]
+    [InlineData(20, "TWENTY DOLLARS")]
+    [InlineData(30, "THIRTY DOLLARS")]
+    [InlineData(40, "FORTY DOLLARS")]
+    [InlineData(50, "FIFTY DOLLARS")]
+    [InlineData(60, "SIXTY DOLLARS")]
+    [InlineData(70, "SEVENTY DOLLARS")]
+    [InlineData(80, "EIGHTY DOLLARS")]
+    [InlineData(90, "NINETY DOLLARS")]
     public void ConvertToWords_TensNumbers_ReturnsCorrectWords(decimal input, string expected)
     {
         var result = _convertService.ConvertCurrencyAmountToWords(input);
@@ -65,10 +65,10 @@ public class ConvertServiceTests
     }
 
     [Theory]
-    [InlineData(1000, "One Thousand Dollars")]
-    [InlineData(1000000, "One Million Dollars")]
-    [InlineData(1000000000, "One Billion Dollars")]
-    [InlineData(1000000000000, "One Trillion Dollars")]
+    [InlineData(1000, "ONE THOUSAND DOLLARS")]
+    [InlineData(1000000, "ONE MILLION DOLLARS")]
+    [InlineData(1000000000, "ONE BILLION DOLLARS")]
+    [InlineData(1000000000000, "ONE TRILLION DOLLARS")]
     public void ConvertToWords_ThousandsNumbers_ReturnsCorrectWords(decimal input, string expected)
     {
         var result = _convertService.ConvertCurrencyAmountToWords(input);
@@ -80,13 +80,13 @@ public class ConvertServiceTests
     [Theory]
     [InlineData(
         1234567890,
-        "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety Dollars"
+        "ONE BILLION TWO HUNDRED THIRTY FOUR MILLION FIVE HUNDRED SIXTY SEVEN THOUSAND EIGHT HUNDRED NINETY DOLLARS"
     )]
-    [InlineData(0.12, "Twelve Cents")]
-    [InlineData(1.55, "One Dollar And Fifty Five Cents")]
-    [InlineData(4.12, "Four Dollars And Twelve Cents")]
-    [InlineData(421.09, "Four Hundred Twenty One Dollars And Nine Cents")]
-    [InlineData(934.01, "Nine Hundred Thirty Four Dollars And One Cent")]
+    [InlineData(0.12, "TWELVE CENTS")]
+    [InlineData(1.55, "ONE DOLLAR AND FIFTY FIVE CENTS")]
+    [InlineData(4.12, "FOUR DOLLARS AND TWELVE CENTS")]
+    [InlineData(421.09, "FOUR HUNDRED TWENTY ONE DOLLARS AND NINE CENTS")]
+    [InlineData(934.01, "NINE HUNDRED THIRTY FOUR DOLLARS AND ONE CENT")]
     public void ConvertToWords_ComplexNumbers_ReturnsCorrectWords(decimal input, string expected)
     {
         var result = _convertService.ConvertCurrencyAmountToWords(input);
@@ -99,10 +99,11 @@ public class ConvertServiceTests
     [InlineData(-4.44)] // Negative numbers
     [InlineData(0.005)] // Does not allow 3rd decimal place
     [InlineData(1144000000000000)] // Too large number
+    [InlineData(1000000000000000)] // Too large number
     public void ConvertToWords_InvalidInputs_ThrowsException(decimal input)
     {
         var act = () => _convertService.ConvertCurrencyAmountToWords(input);
-        act.Should().Throw<ArgumentException>().WithMessage("Invalid input");
+        act.Should().Throw<ArgumentException>().WithMessage("Invalid input: *");
     }
     #endregion
 }
